46fad04c0515e84560bf8b6cc7f2a806a3f377bf
André <andrerrr@outlook.de>
2019-04-16 14:09:05 +0200

    HalloBInding

---
+++ HalloBinding/HalloBinding/BoolToYellowBlueConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace HalloBinding
+{
+    class BoolToYellowBlueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value)
+                return new SolidColorBrush(Colors.Yellow);
+
+            return new SolidColorBrush(Colors.Blue);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
