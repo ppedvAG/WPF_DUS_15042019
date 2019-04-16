b86cfdb77b938c7cbf3f60af0d39a15a8846b773
André <andrerrr@outlook.de>
2019-04-16 15:47:39 +0200

    DataTemples sind toll

--- HalloDaten/HalloDaten/MainWindow.xaml.cs
+++ HalloDaten/HalloDaten/MainWindow.xaml.cs
@@ -1,18 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Newtonsoft.Json;
 using System.Linq;
 using System.Net;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace HalloDaten
 {
@@ -30,9 +20,12 @@ namespace HalloDaten
         {
             using (var wc = new WebClient())
             {
+                wc.Encoding = Encoding.UTF8;
                 var url = $"https://www.googleapis.com/books/v1/volumes?q={suchTb.Text}";
                 var json = wc.DownloadString(url);
-                MessageBox.Show(json);
+                var bookResult = JsonConvert.DeserializeObject<BookResults>(json);
+
+                booksLb.ItemsSource = bookResult.items.Select(x => x.volumeInfo);
             }
         }
     }
