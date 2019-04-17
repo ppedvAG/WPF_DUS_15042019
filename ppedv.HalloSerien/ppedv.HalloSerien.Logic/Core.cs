06088437ec62dbdb14f1c4fdd4f6ad6ca0cde0f4
André <andrerrr@outlook.de>
2019-04-17 15:16:48 +0200

    Consolen UI und Demodaten

--- ppedv.HalloSerien/ppedv.HalloSerien.Logic/Core.cs
+++ ppedv.HalloSerien/ppedv.HalloSerien.Logic/Core.cs
@@ -1,14 +1,79 @@
-﻿using ppedv.HalloSerien.Data.EF;
+﻿using ppedv.HalloSerien.Model;
+using ppedv.HalloSerien.Model.Contracts;
 using System;
 
 namespace ppedv.HalloSerien.Logic
 {
     public class Core
     {
-        public Core()
+        public IRepository Repository { get; private set; }
+
+        public Core(IRepository repository) //<-- Dependency Injection
         {
-            EfContext cccc = new EfContext();
-            cccc.Episodes.
+            Repository = repository;
+        }
+
+        public Core() : this(new Data.EF.EfRepository())
+        { }
+
+        public void CreateDemoData()
+        {
+            var p1 = new Person() { Name = "Fred Feuerstein" };
+            var p2 = new Person() { Name = "Wilma Feuerstein" };
+            var p3 = new Person() { Name = "Barney Geröllheimer" };
+
+
+            var s1 = new Series() { Title = "Game of Drones" };
+            var ep1 = new Episode()
+            {
+                Title = "Auspacken und Aufbauen",
+                Length = TimeSpan.FromMinutes(43),
+                Release = DateTime.Now.AddYears(-3),
+                Season = 1,
+                Director = p1
+            };
+            ep1.Actors.Add(p2);
+            ep1.Actors.Add(p3);
+            s1.Episodes.Add(ep1);
+
+            var ep2 = new Episode()
+            {
+                Title = "Aufladen",
+                Length = TimeSpan.FromMinutes(43),
+                Release = DateTime.Now.AddYears(-3),
+                Season = 1,
+                Director = p2
+            };
+            ep2.Actors.Add(p1);
+            ep2.Actors.Add(p3);
+            s1.Episodes.Add(ep2);
+
+
+            var ep3 = new Episode()
+            {
+                Title = "Starten",
+                Length = TimeSpan.FromMinutes(43),
+                Release = DateTime.Now.AddYears(-3),
+                Season = 1,
+                Director = p3
+            };
+            ep3.Actors.Add(p1);
+            ep3.Actors.Add(p2);
+            s1.Episodes.Add(ep3);
+
+            var ep4 = new Episode()
+            {
+                Title = "Ansturtz",
+                Length = TimeSpan.FromMinutes(43),
+                Release = DateTime.Now.AddYears(-3),
+                Season = 1,
+                Director = p1
+            };
+            ep4.Actors.Add(p1);
+            s1.Episodes.Add(ep4);
+
+            Repository.Add(s1);
+            Repository.SaveAll();
         }
     }
 }
---
+++ ppedv.HalloSerien/ppedv.HalloSerien.UI.DevConsole/Program.cs
@@ -0,0 +1,35 @@
+﻿using ppedv.HalloSerien.Logic;
+using ppedv.HalloSerien.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ppedv.HalloSerien.UI.DevConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** HalloSerien ***");
+
+            var core = new Core();
+            if (core.Repository.Query<Series>().Count() == 0)
+                core.CreateDemoData();
+
+            foreach (var series in core.Repository.Query<Series>().ToList())
+            {
+                Console.WriteLine($"{series.Title}");
+                foreach (var ep in series.Episodes.OrderBy(x => x.Season))
+                {
+                    Console.WriteLine($"\t{ep.Title} {ep.Length.TotalMinutes} Minuten von {ep.Director.Name}");
+                    Console.WriteLine($"\t\t{string.Join(", ", ep.Actors.Select(x => x.Name))}");
+                }
+            }
+
+            Console.WriteLine("Ende");
+            Console.ReadLine();
+        }
+    }
+}
