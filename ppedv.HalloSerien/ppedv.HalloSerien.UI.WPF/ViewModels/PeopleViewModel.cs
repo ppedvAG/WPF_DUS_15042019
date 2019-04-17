ebfc3e41be226664729d4ed722b96b298fb79e38
André <andrerrr@outlook.de>
2019-04-17 16:23:31 +0200

    MVVM für angefangene

---
+++ ppedv.HalloSerien/ppedv.HalloSerien.UI.WPF/ViewModels/PeopleViewModel.cs
@@ -0,0 +1,23 @@
+﻿using ppedv.HalloSerien.Logic;
+using ppedv.HalloSerien.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ppedv.HalloSerien.UI.WPF.ViewModels
+{
+    class PeopleViewModel
+    {
+        public List<Person> PeopleList { get; set; }
+        Core core = new Core();
+
+        public PeopleViewModel()
+        {
+            PeopleList = new List<Person>(core.Repository.Query<Person>());
+        }
+
+
+    }
+}
