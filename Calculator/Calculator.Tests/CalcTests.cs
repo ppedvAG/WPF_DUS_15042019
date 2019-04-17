2c71052a2bba99e314427cac3c3883fde29fa0d7
André <andrerrr@outlook.de>
2019-04-17 11:29:41 +0200

    Tests

---
+++ Calculator/Calculator.Tests/CalcTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calculator.Tests
+{
+    [TestClass]
+    public class CalcTests
+    {
+        [TestMethod]
+        public void Calc_Sum_3_and_2_result_5()
+        {
+            //Arrange
+            Calc calc = new Calc();
+
+            //Act
+            var result = calc.Sum(3, 2);
+
+            //Assert
+            Assert.AreEqual(5, result);
+        }
+
+        [TestMethod]
+        public void Calc_Sum_0_and_0_result_0()
+        {
+            //Arrange
+            Calc calc = new Calc();
+
+            //Act
+            var result = calc.Sum(0, 0);
+
+            //Assert
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void Calc_Sum_MAX_and_1_throws()
+        {
+            Calc calc = new Calc();
+
+            Assert.ThrowsException<OverflowException>(() => calc.Sum(int.MaxValue, 1));
+            Assert.ThrowsException<OverflowException>(() => calc.Sum(int.MinValue, -1));
+
+        }
+    }
+}
---
+++ Calculator/Calculator.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Calculator.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Calculator.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2d9852fe-21e9-4d71-98ca-3ba446302558")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
