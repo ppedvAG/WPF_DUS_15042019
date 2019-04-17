0a7536d2f81026ad082b1feec1dccb3e53d84027
André <andrerrr@outlook.de>
2019-04-17 12:10:18 +0200

    Data Layer mit EF und tests

---
+++ ppedv.HalloSerien/ppedv.HalloSerien.Data.EF.Tests/EfContextTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ppedv.HalloSerien.Data.EF.Tests
+{
+    [TestClass]
+    public class EfContextTests
+    {
+        [TestMethod]
+        public void EfContext_can_create_database()
+        {
+            using (var con = new EfContext())
+            {
+                if (con.Database.Exists())
+                    con.Database.Delete();
+
+                con.Database.Create();
+                Assert.IsTrue(con.Database.Exists());
+            }
+        }
+    }
+}
---
+++ ppedv.HalloSerien/ppedv.HalloSerien.Data.EF/EfContext.cs
@@ -0,0 +1,26 @@
+﻿using ppedv.HalloSerien.Model;
+using System.Data.Entity;
+
+namespace ppedv.HalloSerien.Data.EF
+{
+    public class EfContext : DbContext
+    {
+        public DbSet<Episode> Episodes { get; set; }
+        public DbSet<Person> People { get; set; }
+        public DbSet<Series> Series { get; set; }
+
+        public EfContext(string conString) : base(conString)
+        { }
+
+      // //Express
+      // public EfContext() : this("Server=.\\SQLEXRESS;Database=HalloSerien_dev;Trusted_Connection=true")
+      // { }
+      //
+      // //localdb
+      // public EfContext() : this("Server=(localdb)\\mssqllocaldb;Database=HalloSerien_dev;Trusted_Connection=true")
+      // { }
+        //VOLL
+        public EfContext() : this("Server=.;Database=HalloSerien_dev;Trusted_Connection=true")
+        { }
+    }
+}
