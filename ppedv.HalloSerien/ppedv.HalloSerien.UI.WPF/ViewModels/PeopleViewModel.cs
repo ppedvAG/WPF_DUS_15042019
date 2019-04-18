03a2141a400899d6cf8f1f74df44e65fc9f01794
André <andrerrr@outlook.de>
2019-04-18 15:14:32 +0200

    Löschen mit MessageBox

--- ppedv.HalloSerien/ppedv.HalloSerien.UI.WPF/ViewModels/PeopleViewModel.cs
+++ ppedv.HalloSerien/ppedv.HalloSerien.UI.WPF/ViewModels/PeopleViewModel.cs
@@ -10,7 +10,8 @@ namespace ppedv.HalloSerien.UI.WPF.ViewModels
     {
         Core core = new Core();
         private Person selectedPerson;
-
+        private bool showDialog;
+        private string dlgText;
 
         public ObservableCollection<Person> PeopleList { get; set; }
 
@@ -51,6 +52,31 @@ namespace ppedv.HalloSerien.UI.WPF.ViewModels
         public ICommand SaveCommand { get; set; }
         public ICommand NewCommand { get; set; }
 
+        public ICommand DeleteCommand { get; set; }
+
+        public string DlgText
+        {
+            get => dlgText;
+            set
+            {
+                dlgText = value;
+                IChanged();
+            }
+        }
+        public bool ShowDialog
+        {
+            get => showDialog;
+            set
+            {
+                showDialog = value;
+                IChanged();
+            }
+        }
+        public ICommand UserConfirmedCommand { get; set; }
+        public ICommand UserDeniedCommand { get; set; }
+
+
+
         public PeopleViewModel()
         {
             PeopleList = new ObservableCollection<Person>(core.Repository.Query<Person>());
@@ -59,6 +85,31 @@ namespace ppedv.HalloSerien.UI.WPF.ViewModels
             SaveCommand = new RelayCommand(o => core.Repository.SaveAll());
 
             NewCommand = new RelayCommand(UserWantsToAddNewPerson);
+
+            DeleteCommand = new RelayCommand(UserWantsToDeleteSelectedPerson);
+
+            UserConfirmedCommand = new RelayCommand(DeleteSelectedPerson);
+            UserDeniedCommand = new RelayCommand(x => ShowDialog = false);
+        }
+
+        private void UserWantsToDeleteSelectedPerson(object obj)
+        {
+            if (SelectedPerson != null)
+            {
+                DlgText = $"Soll der Mensch {SelectedPerson.Name} wirlich gelöscht werden";
+                ShowDialog = true;
+            }
+        }
+
+        private void DeleteSelectedPerson(object obj)
+        {
+            ShowDialog = false;
+
+            if (SelectedPerson != null)
+            {
+                core.Repository.Delete(selectedPerson);
+                PeopleList.Remove(selectedPerson);
+            }
         }
 
         private void UserWantsToAddNewPerson(object obj)
@@ -66,7 +117,6 @@ namespace ppedv.HalloSerien.UI.WPF.ViewModels
             var p = new Person() { Name = "NEU", BirthDate = DateTime.Now };
             core.Repository.Add(p);
             PeopleList.Add(p);
-
         }
     }
 }
