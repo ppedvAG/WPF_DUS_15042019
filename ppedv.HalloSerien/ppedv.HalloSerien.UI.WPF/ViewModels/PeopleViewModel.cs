8f2af1b095a5b4761fee3e17abcf4b5e4bc20819
André <andrerrr@outlook.de>
2019-04-18 12:11:10 +0200

    PersonItem für GUI als Wrapper für Person(Domain)

--- ppedv.HalloSerien/ppedv.HalloSerien.UI.WPF/ViewModels/PeopleViewModel.cs
+++ ppedv.HalloSerien/ppedv.HalloSerien.UI.WPF/ViewModels/PeopleViewModel.cs
@@ -2,6 +2,7 @@
 using ppedv.HalloSerien.Model;
 using System;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Windows.Input;
 
 namespace ppedv.HalloSerien.UI.WPF.ViewModels
@@ -9,35 +10,35 @@ namespace ppedv.HalloSerien.UI.WPF.ViewModels
     class PeopleViewModel : ViewModelBase
     {
         Core core = new Core();
-        private Person selectedPerson;
+        private PersonItem selectedPerson;
 
 
-        public ObservableCollection<Person> PeopleList { get; set; }
+        public ObservableCollection<PersonItem> PeopleList { get; set; }
 
-        public Person SelectedPerson
+        public PersonItem SelectedPerson
         {
             get => selectedPerson;
             set
             {
                 selectedPerson = value;
                 OnPropertyChanged(nameof(Age));
-                OnPropertyChanged(nameof(BirthDate));
+                //OnPropertyChanged(nameof(BirthDate));
                 IChanged();
             }
         }
 
-        public DateTime BirthDate
-        {
-            get => selectedPerson == null ? DateTime.MaxValue : selectedPerson.BirthDate;
-            set
-            {
-                if (selectedPerson != null)
-                    selectedPerson.BirthDate = value;
+        //public DateTime BirthDate
+        //{
+        //    get => selectedPerson == null ? DateTime.MaxValue : selectedPerson.BirthDate;
+        //    set
+        //    {
+        //        if (selectedPerson != null)
+        //            selectedPerson.BirthDate = value;
 
-                OnPropertyChanged("");//update all
+        //        OnPropertyChanged("");//update all
 
-            }
-        }
+        //    }
+        //}
 
         public int Age
         {
@@ -53,7 +54,8 @@ namespace ppedv.HalloSerien.UI.WPF.ViewModels
 
         public PeopleViewModel()
         {
-            PeopleList = new ObservableCollection<Person>(core.Repository.Query<Person>());
+            PeopleList = new ObservableCollection<PersonItem>();
+            core.Repository.Query<Person>().ToList().ForEach(x => PeopleList.Add(new PersonItem(x)));
             SaveCommandOldSchool = new SaveCommand(core);
 
             SaveCommand = new RelayCommand(o => core.Repository.SaveAll());
@@ -65,8 +67,9 @@ namespace ppedv.HalloSerien.UI.WPF.ViewModels
         {
             var p = new Person() { Name = "NEU", BirthDate = DateTime.Now };
             core.Repository.Add(p);
-            PeopleList.Add(p);
-
+            var pi = new PersonItem(p);
+            PeopleList.Add(pi);
+            SelectedPerson = pi;
         }
     }
 }
---
+++ ppedv.HalloSerien/ppedv.HalloSerien.UI.WPF/ViewModels/PersonItem.cs
@@ -0,0 +1,51 @@
+﻿using ppedv.HalloSerien.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ppedv.HalloSerien.UI.WPF.ViewModels
+{
+    class PersonItem : ViewModelBase
+    {
+        private Person person;
+
+        public Person Person
+        {
+            get => person;
+            set
+            {
+                person = value;
+                IChanged();
+            }
+        }
+
+
+        public string Name
+        {
+            get { return person.Name; }
+            set
+            {
+                person.Name = value;
+                IChanged();
+            }
+        }
+
+        public DateTime BirthDate
+        {
+            get { return person.BirthDate; }
+            set
+            {
+                person.BirthDate = value;
+                IChanged();
+            }
+        }
+
+
+        public PersonItem(Person person)
+        {
+            Person = person;
+        }
+    }
+}
